0e9c674e53f1a6578feb71d4f9ec328fe9fe506d
Risul Islam <risul@regfire.no>
2014-12-30 23:48:25 +0100

    Make mime available on global scope

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: 'risul:aws-s3direct',
     summary: 'Meteor package for generating s3 Upload and Download Policies for direct upload/download',
-    version: '1.0.9',
+    version: '1.0.10',
     git: 'https://github.com/risul/meteor-aws-s3direct'
 });
 
@@ -12,5 +12,6 @@ Npm.depends({
 Package.on_use(function (api) {
     api.versionsFrom('METEOR@0.9.2.2');
     api.export('s3Direct');
+    api.export('mime');
     api.add_files('lib/s3Direct.js', 'server');
 });
