02b2ef686466dbd656faa4a381f56d3be031bc9c
Risul Islam <risul.islam@regfire.com>
2014-12-30 19:16:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Easy generate AWS S3 upload & download policy gen
+===============
+
+More info coming soon
---
+++ package.js
@@ -0,0 +1,16 @@
+Package.describe({
+    name: 'risul:aws-s3direct',
+    summary: 'Meteor package for generating s3 Upload and Download Policies for direct upload/download',
+    version: '1.0.9',
+    git: 'https://github.com/risul/meteor-aws-s3direct'
+});
+
+Npm.depends({
+    "mime": '1.2.11'
+});
+
+Package.on_use(function (api) {
+    api.versionsFrom('METEOR@0.9.2.2');
+    api.export('s3Direct');
+    api.add_files('lib/s3Direct.js', 'server');
+});
