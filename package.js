02ca036fff0f1b8730c678f71f7088b624a746de
root <risul.islam@regfire.com>
2015-07-28 18:57:50 +0200

    Rewrite getSignedUrl method to use the official AWS-SDK (AWS.S3.getSignedUrl)

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: 'risul:aws-s3direct',
-    summary: 'Meteor package for generating s3 Upload and Download Policies for direct upload/download',
-    version: '1.1.0',
+    summary: 'Meteor package for generating s3 Upload policy and file url for direct upload/view of files',
+    version: '1.1.3',
     git: 'https://github.com/risul/meteor-aws-s3direct'
 });
 
@@ -11,6 +11,7 @@ Npm.depends({
 
 Package.on_use(function (api) {
     api.versionsFrom('METEOR@1.0');
+    api.use("risul:aws-sdk@1.0.6", "server")
     api.export('s3Direct');
     api.export('mime');
     api.add_files('lib/s3Direct.js', 'server');
