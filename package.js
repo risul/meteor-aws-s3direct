e8942da77e1a12a515187209f7f9268a0904b0e4
root <risul.islam@regfire.com>
2015-07-28 11:12:00 +0200

    Updated mime NPM +  added support for env variables

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
     name: 'risul:aws-s3direct',
     summary: 'Meteor package for generating s3 Upload and Download Policies for direct upload/download',
-    version: '1.0.10',
+    version: '1.1.0',
     git: 'https://github.com/risul/meteor-aws-s3direct'
 });
 
 Npm.depends({
-    "mime": '1.2.11'
+    "mime": '1.3.4'
 });
 
 Package.on_use(function (api) {
-    api.versionsFrom('METEOR@0.9.2.2');
+    api.versionsFrom('METEOR@1.0');
     api.export('s3Direct');
     api.export('mime');
     api.add_files('lib/s3Direct.js', 'server');
